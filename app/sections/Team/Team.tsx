ed854cc58dbeb33674e6484d02ee41d969c6e4b3
Annie Li <108150567+anccaz@users.noreply.github.com>
2024-10-08 15:18:18 -0500

    Meet the Team header added

---
+++ app/sections/Team/Team.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Team.css'
+
+function Team() {
+  return (
+      <div className="team-header">
+          <h1 className='team-title text-[#FFE9D7] text-center text-4xl md:text-5xl lg:text-5xl pb-7 md:pb-15 md:pb-20'>Meet the Team</h1>
+      </div>  
+  )
+}
+
+export default Team
