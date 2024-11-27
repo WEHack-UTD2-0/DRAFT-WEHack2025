f815aeda90bfe5e12e7e25b83ed5a89c88c9081e
saf-s <78832241+saf-s@users.noreply.github.com>
2024-11-27 10:35:03 -0600

    Updated about and sponsor sections, added new theme
    Updated about section (new theme and mascots), updated sponsor section (new theme) and hid sample sponsors

--- app/sections/About/About.tsx
+++ app/sections/About/About.tsx
@@ -4,22 +4,25 @@ import Stats from '@/app/components/Stats/Stats'
 
 function About() {
   return (
-    <section className='about-section' id="about-wehack">
-      {/* group 1 */}
-      <div className='flex justify-center items-center'>
-          <img className="h-auto md:px-0" src={"/static/images/Group 34.svg"} alt={"Dallas' Largest Inclusive Hackathon"} />
+    <section className='parent-container about-section bg-[#251E2B]' id="about-wehack">
+      <div className='about-title flex justify-center items-center'>
+        <img src={"/static/images/about-title.png"} alt={"Dallas' Largest Inclusive Hackathon"} />
       </div>
-      <div className='about-group flex flex-wrap flex-col sm:flex-row md:flex-row: lg:flex-row'>
-        <div className='about-text-container flex flex-col items-center justify-center gap-y-5 -mt-10 md:mt-5 lg:mt-20'>
-          <p className='about-text text-[#CCBA97] font-medium text-lg md:text-xl lg:text-2xl'>The purpose of WEHack is to create an inclusive and encouraging environment for women, nonbinary genders, and all underrepresented groups in technology so that they can gain the skills and confidence to excel in future hackathons and the tech world.</p>
-          <p className='about-text text-[#CCBA97] font-medium text-lg md:text-xl lg:text-2xl'>We aim to encourage more underrepresented individuals to harness their skills by creating software or hardware projects of their own. Whether it's networking with sponsors and hackers, gaining development and workshop experience, or competing against other teams for challenges and prizes, WEHack offers participants an opportunity to end the weekend with unique takeaways.</p>
+      <div className='about-text-container flex flex-col items-center justify-center gap-y-5'>
+        <p className='about-text text-[#CCBA97] font-medium text-lg lg:text-xl'>The purpose of WEHack is to create an inclusive and encouraging environment for women, nonbinary genders, and all underrepresented groups in technology so that they can gain the skills and confidence to excel in future hackathons and the tech world.</p>
+        <p className='about-text text-[#CCBA97] font-medium text-lg lg:text-xl'>We aim to encourage more underrepresented individuals to harness their skills by creating software or hardware projects of their own. Whether it's networking with sponsors and hackers, gaining development and workshop experience, or competing against other teams for challenges and prizes, WEHack offers participants an opportunity to end the weekend with unique takeaways.</p>
+      </div>
+ 
+      {/* film strip */}
+      <div className="film-strip-container">
+        <img src="/static/images/about-film-strip-photos.png" className="film-strip" />
+        {/*<div className="scrolling-images">
+          <img src="/static/images/IMG_0165.jpg" alt="a group of people at a sponsor fair" loading="eager" />
+          <img src="/static/images/IMG_1752.jpg" alt="two girls smiling at the camera, holding a WEHack tote bag" loading="eager" />
+          <img src="/static/images/IMG_0165.jpg" alt="a group of people at a sponsor fair" loading="eager" />
         </div>
-      </div>
-
-      {/* <div className="film-strip-container">
-        <img src="/static/images/Film Strip.svg" className="film-strip" />
-      </div> */}
-    
+        */}
+        </div>
 
       {/* stats */}
       {/* <Stats/> */}
--- app/sections/Sponsor/Sponsor.tsx
+++ app/sections/Sponsor/Sponsor.tsx
@@ -12,17 +12,16 @@ const sponsors = [
 
 function Sponsor() {
     return (
-      <div className='Sponsor-container w-full h-auto flex flex-wrap flex-col items-center justify-center p-6 pb-40 lg:pb-60' id="Sponsor">
-        <div className="flex flex-col md:flex-row justify-center items-center gap-y-2 md:gap-x-16 lg:gap-x-24 pb-24 md:pb-24 lg:pb-36">
-          <div className="glow-sponsor">
-            <h1 className='FAQ-title text-[#992444] text-center text-4xl md:text-5xl lg:text-5xl'>2025 Sponsors</h1>
-          </div>
+      <div className='Sponsor-container bg-[#251E2B] w-full h-auto flex flex-wrap flex-col items-center justify-center p-6 pb-30 lg:pb-30' id="Sponsor">
+        <div className="flex flex-row items-center justify-items-center justify-center">
+                <img className="sponsor-title w-auto max-h-40 m-12" src={"/static/images/sponsors-title.png"} alt={"sponsors"}/>
         </div>
-        <div className="sponsor-text-container flex flex-col gap-y-5 flex-wrap items-center justify-center">
-          <p className='sponsor-text text-[#CCBA97] font-medium text-4xl md:text-5xl'>Coming Soon</p>
-          <p className='sponsor-text text-[#CCBA97] font-medium text-2xl md:text-3xl text-center'>Interested in sponsoring WEHack 2025? Contact us at wehackutd@gmail.com!</p>
+        <div className="sponsor-text-container flex flex-wrap items-center justify-center">
+        <p className='sponsor-text text-[#CCBA97] font-medium text-lg lg:text-xl'>Interested in sponsoring WEHack 2025? Contact us at wehackutd@gmail.com!</p>
         </div>
-        {/* <div className='Sponsor-logos flex flex-wrap justify-around w-full'>
+        {/*
+        <h1 className='Sponsor-title text-[#CCBA97] text-center text-4xl md:text-5xl lg:text-5xl pb-7 md:pb-15 md:pb-20'>2025 Sponsors</h1>
+        <div className='Sponsor-logos flex flex-wrap justify-around w-full'>
           {sponsors.map((logo, index) => (
             <div key={index} className='Sponsor-logo bg-[#FFE9D7] rounded-lg flex items-center justify-center' style={{ '--hover-image': `url(${logo.hover})` } as React.CSSProperties}>
               <img src={logo.default} alt={`Sponsor ${index + 1}`} className='sponsor-img' />
@@ -32,6 +31,7 @@ function Sponsor() {
         <div className="vinyl-yellow-container flex justify-center md:justify-start pt-20 pb-72 md:pt-30 md:pb-64 md:-mr-[5rem] lg:-mr-[20rem] xl:-mr-[35rem]">
           <img className="vinyl-yellow overflow-hidden" src="/static/images/Untitled_Artwork 5.png" width="250px" height="250px"></img>    
         </div>
+        */}
       </div>
     );
   }
